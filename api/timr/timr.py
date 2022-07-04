1ea07d84dd4d1c81aacac0cf9d1451cbd39b9c68
Amin Chirazi <aminchirazi@Amins-MacBook-Pro-2.local>
2022-07-04 17:32:08 +0200

    timr filters added

--- api/timr/timr.py
+++ api/timr/timr.py
@@ -5,17 +5,18 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    startdate = req.params.get('startdate')
+    enddate = req.params.get('enddate')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('startdate')
 
-    if name:
-        return func.HttpResponse("Hello, {name}. This HTTP triggered function executed successfully.")
+    if startdate:
+        return func.HttpResponse("Start Date: " + startdate + "\nEnd Date: " + enddate)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
